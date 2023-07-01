409fe0d747309724ac16811eceaf28ca79ae0692
nirgluzman <nir.gluzman@gmail.com>
2023-07-02 01:42:34 +0200

    update middyfy to use Middy to automatically adds CORS headers in the response

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -2,7 +2,8 @@ import { Handler } from 'aws-lambda';
 
 import middy from '@middy/core';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
+import cors from '@middy/http-cors';
 
 export const middyfy = (handler: Handler) => {
-  return middy(handler).use(middyJsonBodyParser());
+  return middy(handler).use(middyJsonBodyParser()).use(cors());
 };
